cea8125a17c2c63b77d8e64123abb9627ebf6766
Johnebhin <121502677+Johnebhin@users.noreply.github.com>
2023-03-28 19:58:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,24 +8,23 @@ var canvas;
 var palyer, playerBase;
 var computer, computerBase;
 
+var arrow;
+
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  
 
   engine = Engine.create();
   world = engine.world;
 
   playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
-//create a player object from the Player class.
-player=new Player(
-  280,
-  playerBase.body.position.y -153,
-  50,
-  180
-  
-)
-
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
 
   computerBase = new ComputerBase(
     width - 300,
@@ -39,13 +38,19 @@ player=new Player(
     50,
     180
   );
+  computerArcher = new ComputerArcher(
+    width - 340,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
   
-
+  arrow = new PlayerArrow(playerArcher.body.position.x, playerArcher.body.position.y, 100, 10);
   
 }
 
 function draw() {
-  background(189);
+  background(180);
 
   Engine.update(engine);
 
@@ -57,11 +62,29 @@ function draw() {
 
  
   playerBase.display();
-//call the display() function for the player object.
   player.display();
+  
 
   computerBase.display();
   computer.display();
   
+  playerArcher.display();
+  computerArcher.display()
+  
+   //if(keyCode === 32){
+     //arrow.display()
+    //arrow.shoot(playerArcher);
+   // }
 
+   // if(keyCode === 32){
+    // arrow.shoot(playerArcher.body.angle);
+  //  }
+   
+   if(keyCode === 32){
+     arrow.display()
+    arrow.shoot(playerArcher.body.angle);
+   }
 }
+
+
+
